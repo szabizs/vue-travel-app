d3968b6280f27a8acd403844915ec718e8965496
Madalin Popa <contact@madalinpopa.com>
2020-09-18 00:37:13 +0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Jamaica from "../views/Jamaica.vue";
-import Panama from "../views/Panama.vue";
 
 Vue.use(VueRouter);
 
@@ -13,39 +9,22 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    props: true,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/details/:slug",
+    name: "DestinationDetails",
+    props: true,
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: Brazil,
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: Hawaii,
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: Jamaica,
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: Panama,
+      import(
+        /* webpackChunkName: DestinationDetails */ "../views/DestinationDetails.vue"
+      ),
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
+  linkExactActiveClass: "vue-school-active-class",
   routes,
 });
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <div v-for="destination in destinations" v-bind:key="destination.name">
+        <router-link
+          :to="{
+            name: 'DestinationDetails',
+            params: { slug: destination.slug },
+          }"
+        >
+          <h2>{{ destination.name }}</h2>
+        </router-link>
+        <figure>
+          <router-link
+            :to="{
+              name: 'DestinationDetails',
+              params: { slug: destination.slug },
+            }"
+          >
+            <img
+              :src="require(`@/assets/${destination.image}`)"
+              :alt="destination.name"
+            />
+          </router-link>
+        </figure>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,8 +38,33 @@ export default {
   components: {},
   data() {
     return {
-      destination: store.destination,
+      destinations: store.destinations,
     };
   },
 };
 </script>
+
+<style scoped>
+.home {
+  max-width: 1140px;
+  margin: 0 auto;
+}
+
+img {
+  max-width: 200px;
+}
+
+.destinations {
+  display: flex;
+  justify-content: space-between;
+}
+
+a {
+  color: lightseagreen;
+  text-decoration: none;
+}
+a:hover,
+a:visited {
+  text-decoration: underline;
+}
+</style>
