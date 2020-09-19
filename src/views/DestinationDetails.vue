6a1bc0bdee1c31fa5fc619e6694a7124ffba75d9
Madalin Popa <contact@madalinpopa.com>
2020-09-20 01:24:44 +0300

    add goBack button

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoBack />
     <section class="destination">
       <h1>{{ destination.name }}</h1>
       <div class="destination-details">
@@ -41,9 +42,13 @@
 
 <script>
 import store from "@/store.js";
+import GoBack from "@/components/GoBack";
 export default {
   data() {
     return {};
+  },
+  components: {
+    GoBack,
   },
   props: {
     slug: {
